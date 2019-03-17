5f21c200e297756276b3d43c046374e1745554fe
Rohan Parekh <rohan.parekh.in@gmail.com>
2019-03-17 22:20:51 +0100

    fixed rounding logic

--- src/main/java/org/myworks/service/CalculationService.java
+++ src/main/java/org/myworks/service/CalculationService.java
@@ -4,13 +4,11 @@ import org.myworks.constants.TaxServiceContants;
 import org.myworks.model.Order;
 import org.myworks.model.Product;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 public class CalculationService {
 
-    private double totalSalesTax;
+    private double totalBill = 0.00;
 
     private double roundedDouble = 0.0;
 
@@ -19,47 +17,36 @@ public class CalculationService {
     }
 
     private double calculateTax(Order order) {
-        double totalPrice = 0.00;
-        double taxPerItem;
+        double itemPrice;
         double importedTax;
-        double salesTax=0.00;
 
         List<Product> products = order.getProducts();
 
         for (Product product : products) {
             if (product.isTaxable()) {
-                taxPerItem = product.getProductPrice() + product.getProductPrice() * TaxServiceContants.SALES_TAX_AMOUNT;
+                itemPrice = product.getProductPrice() + product.getProductPrice() * TaxServiceContants.SALES_TAX_AMOUNT;
             } else {
-                taxPerItem = product.getProductPrice();
+                itemPrice = product.getProductPrice();
             }
+
+            itemPrice = calculateRoundedTax(itemPrice);
+
             if (product.isImported()) {
-                importedTax = product.getProductPrice() * TaxServiceContants.IMPORTED_TAX_AMOUNT;
-                taxPerItem = taxPerItem + importedTax;
+                importedTax = calculateRoundedTax(product.getProductPrice() * TaxServiceContants.IMPORTED_TAX_AMOUNT);
+                itemPrice = itemPrice + importedTax;
             }
-            totalPrice = totalPrice + taxPerItem;
+            totalBill = totalBill + itemPrice;
         }
-
-        return calculateRoundedAmount(totalPrice);
-
+        return totalBill;
     }
 
-    public double calculateRoundedAmount(double totalPrice) {
-        roundedDouble = Math.round(totalPrice * 100) / 100.0; // round up to multiple of 0.05
-        return roundedDouble;
+    private double calculateRoundedTax(double salesTax) {
+        double roundToTwoDedimal = Math.round(salesTax * 100) / 100.0; // round to two decimal
+        return Math.round(roundToTwoDedimal * 20) / 20.0; // round up to multiple of 0.05
     }
 
-//    public boolean isRoundingNeeded(BigDecimal totalPrice) {
-//        totalPrice = totalPrice.setScale(2, RoundingMode.CEILING);
-//        BigDecimal fractionalPart = totalPrice.remainder( BigDecimal.ONE ); // Result:  0.4523434
-//        double checkDecimalFraction = fractionalPart.setScale(2, RoundingMode.CEILING).doubleValue() / 0.05;
-//        if(checkDecimalFraction == 0.99 || checkDecimalFraction == 0.00) {
-//            return false;
-//        }
-//        return true;
-//    }
-
-    public double getTotalSalesTax() {
-        return totalSalesTax;
+    public double getTotalBill() {
+        return totalBill;
     }
 
     public double getRoundedDouble() {
